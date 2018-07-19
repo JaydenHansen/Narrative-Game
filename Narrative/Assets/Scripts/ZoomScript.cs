317ee352f75848a59c3341ba98cc7ae1153f01c7
JaydenHansen <39174579+JaydenHansen@users.noreply.github.com>
2018-07-19 22:30:57 +1000

    Added a fire particle, added lighting and animations for a flicker. rearanged the level.

---
+++ Narrative/Assets/Scripts/ZoomScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomScript : MonoBehaviour {
+
+    public Camera myCamera;
+    public float maxZoom;
+    public float zoomSpeed;
+
+    float defultZoom;
+    float targetZoom;
+
+	// Use this for initialization
+	void Start () {
+
+        defultZoom = myCamera.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1))
+        {
+            targetZoom = maxZoom;
+        }
+        else
+        {
+            targetZoom = defultZoom;
+        }
+
+        myCamera.fieldOfView = Mathf.Lerp(myCamera.fieldOfView, targetZoom, Time.deltaTime * zoomSpeed);
+	}
+}
